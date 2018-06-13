fdd1aad9027ce011ef4a8569b839ce6dbb11182c
Kevin Shi <kevin.shi@wdc.com>
2018-06-13 10:14:26 -0700

    add functionality to deal with errors

--- move tool.bat
+++ move tool.bat
@@ -1,8 +1,19 @@
-@echo on
+@echo off
 break=on
 
 :begin
 rem number - 1 = time in seconds to wait
-PING localhost -n 6 >NUL 
-move thing1, thing2
+PING localhost -n 6 >NUL
+if not exist #file#(
+   goto error
+) else ( 
+   copy #file#, #destination#
+)
 goto begin
+
+:error
+echo. File does not exist. Prss any key to continune when file exists.
+pause
+goto begin
+
+exit
