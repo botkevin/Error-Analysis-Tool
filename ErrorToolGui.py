b865479c9d28c31431e681ef46c10a22f1bb6d87
Kevin Shi <kevin.shi@wdc.com>
2018-06-22 14:40:52 -0700

    add some documentation

--- ErrorToolGui.py
+++ ErrorToolGui.py
@@ -1,14 +1,20 @@
+#@author Kevin Shi
+#VERY basic gui to make it so any operator can use this program without any prior training
+#first ensures that you have entered the floppy and mounts it
+#then assks the tool and stores the data into one of these tools.
+
 import tkinter as tk
 import err_parse
 import subprocess
 
+#main page and the widgets
 window = tk.Tk()
 window.title("Error Parsing Tool")
 lbl = tk.Label(window, text = "Please insert floppy:", font = "Arial 30")
 lbl.grid(row = 0, column = 0, columnspan=3, padx = 100, pady = 100)
 path_to_mount_script = '' #TODO: Insert path here
 
-
+#start page when you run the script
 def first_page():
 	def clicked():
 		print("oi")
@@ -19,7 +25,7 @@ def first_page():
 	btn.grid(row = 1, column = 1, pady = 150)
 	window.mainloop()
 
-
+#page of the which tool it comes from
 def second_page():
 	lbl.config(text="Please choose tool:")
 	def changeTool(num):
@@ -30,11 +36,13 @@ def second_page():
 	btn1.grid(row = 1, column = 0, pady = 150)
 	btn2.grid(row = 1, column = 1, pady = 150)
 	btn3.grid(row = 1, column = 2, pady = 150)
+
 #clears the window
 def forget():
 	for widget in window.winfo_children():
 		widget.grid_forget()
 
+#Parses the data using err_parse.py
 #TODO: complete where to store data
 #TODO: complete the file name to find data
 def parse_v(tool_num):
