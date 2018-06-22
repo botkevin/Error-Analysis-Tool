d303148a3a26aed828fc3cd829fce7191ee98268
Kevin Shi <kevin.shi@wdc.com>
2018-06-22 11:46:08 -0700

    Add GUI to tool and script to mount floppy.

---
+++ ErrorToolGui.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+import err_parse
+
+window = tk.Tk()
+window.title("Error Parsing Tool")
+lbl = tk.Label(window, text = "Please insert floppy:", font = "Arial 30")
+lbl.grid(row = 0, column = 0, columnspan=3, padx = 100, pady = 100)
+path_to_mount_script = '' #TODO: Insert path here
+
+def first_page():
+	def clicked():
+		print("oi")
+		btn.grid_forget()
+		second_page()
+		subprocess.Popen(["bash", path_to_mount_script])
+	btn = tk.Button(window, text = "Floppy is inserted", command = clicked, font = "Arial 20")
+	btn.grid(row = 1, column = 1, pady = 150)
+	window.mainloop()
+
+
+def second_page():
+	lbl.config(text="Please choose tool:")
+	def changeTool(num):
+		parse_v(num)
+	btn1 = tk.Button(window, text = "HHTRIE01", command = lambda: changeTool(1), font = "Arial 20")
+	btn2 = tk.Button(window, text = "HHTRIE02", command = lambda: changeTool(2), font = "Arial 20")
+	btn3 = tk.Button(window, text = "HHTRIE03", command = lambda: changeTool(3), font = "Arial 20")
+	btn1.grid(row = 1, column = 0, pady = 150)
+	btn2.grid(row = 1, column = 1, pady = 150)
+	btn3.grid(row = 1, column = 2, pady = 150)
+
+def forget():
+	for widget in window.winfo_children():
+		widget.grid_forget()
+
+def parse_v(tool_num):
+	forget()
+	print(tool_num)
+        
+
+first_page()
---
+++ mount_f.sh
@@ -0,0 +1,22 @@
+# make sure the mount point exists
+if ! [ -e /media/floppy ]; then
+    sudo mkdir /media/floppy
+fi
+
+# get a list of the available disks
+disks=($(udisks --enumerate |\
+         sed 's_/org/freedesktop/UDisks/devices/__' |\
+         grep 'sd'))
+
+#get a list of mounted devices
+mounts=($(mount | grep '/dev/sd' | awk '{print $1}'))
+
+# work out which disk is not mounted (first one found - assume this is the fdd)
+for disk in "${disks[@]}"; do
+    if ! for mount in "${mounts[@]}"; do echo $mount; done | grep -q $disk 
+    then
+        sudo mount -t vfat /dev/$disk /media/floppy -o uid=1000
+        echo fdd /dev/$disk mounted on /media/floppy
+        break
+    fi
+done
