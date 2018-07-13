253a75b8ae3cfb8dbf905dd2d5ce60c6f15569e8
botkevin <35553770+botkevin@users.noreply.github.com>
2018-07-13 13:02:54 -0700

    Change mount_f.sh to take parameter of port name

--- umount_f.sh
+++ umount_f.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # unmount a floppy drive mounted on /media/floppy
 
-sudo umount /media/floppy
-sudo rmdir /media/floppy
-echo fdd unmounted from /media/floppy
+sudo umount /media/$1
+sudo rmdir /media/$1
+echo fdd unmounted from /media/$1
